f0eb66d3cb4021c1d978e08d73c0adf7a5ab1d10
l1wobak <l1wobak@edu.aau.at>
2024-03-29 09:52:12 +0100

    added the counting of chars and sorting thereof

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import java.io.*;
 import java.net.URL;
 import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     private static final String URL = "https://www.w3.org/TR/2003/REC-PNG-20031110/iso_8859-1.txt";
@@ -15,17 +19,28 @@ public class Main {
 
             URL url = new URL(URL);
             BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-
             BufferedWriter writer = new BufferedWriter(new FileWriter(SAVEPATH));
 
             String line;
             int counter=0;
+            char[] charArray;
+            Map<Character, Integer> charCountMap = new HashMap<>();
             while ((line = reader.readLine()) != null) {
                 writer.write(line);
                 System.out.format("Line#%d: %s \n", counter++, line);
                 writer.newLine();
+                charArray=line.toCharArray();
+                for (char c : charArray){
+                    charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
+                }
             }
 
+            List<Map.Entry<Character, Integer>> sortedEntries = new ArrayList<>(charCountMap.entrySet());
+            sortedEntries.sort(Map.Entry.<Character, Integer>comparingByValue().reversed());
+
+            for (Map.Entry<Character, Integer> entry : sortedEntries) {
+                System.out.format("%s: %d \n", entry.getKey(), entry.getValue());
+            }
 
             reader.close();
             writer.close();
@@ -33,8 +48,5 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-
-
 }
